ee081846d549b45432f2ea1e032f87125cae5cd9
fatih <mfatihbakir@gmail.com>
2017-06-02 20:18:55 +0300

    windows support

--- include/malt/engine_defs.hpp
+++ include/malt/engine_defs.hpp
@@ -4,7 +4,11 @@
 
 #pragma once
 
-#define MALT_WEAK_SYMBOL __attribute((weak))
+#if !defined(MALT_STATIC_LIBS) && !defined(MALT_GAME_IMPL)
+#define MALT_WEAK_SYMBOL __attribute__((weak))
+#else
+#define MALT_WEAK_SYMBOL
+#endif
 
 #define MALT_PUBLIC __attribute__((visibility("default")))
 #define MALT_PRIVATE __attribute__((visibility("hidden")))
