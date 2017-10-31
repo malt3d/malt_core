c86833acea58c67ee563fe96c0244c2828167f8c
Fatih BAKIR <mfatihbakir@gmail.com>
2017-10-31 14:49:38 -0700

    fix

--- include/malt/malt_fwd.hpp
+++ include/malt/malt_fwd.hpp
@@ -3,12 +3,13 @@
 //
 
 #include <cstdint>
+#include <cstddef>
 
 namespace malt
 {
     using entity_id = uint32_t;
     using comp_index_t = uint32_t;
-    using comp_hash_t = size_t;
+    using comp_hash_t = std::size_t;
     using module_id = uint32_t;
 
     class entity;
